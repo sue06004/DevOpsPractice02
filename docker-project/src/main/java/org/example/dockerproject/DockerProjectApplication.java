9bd71506bd1f0faac498569533ff72942ec91fcb
sue06004 <sue06004@naver.com>
2024-08-19 12:11:31 +0900

    [Init] fist commit

---
+++ docker-project/src/main/java/org/example/dockerproject/DockerProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerProjectApplication.class, args);
+    }
+
+}
---
+++ docker-project/src/test/java/org/example/dockerproject/DockerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dockerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
